2cacf2f0420ba6468205df5c993d62d75ff945c6
Tyler Betzler <Tyler.J.Betzler@gmail.com>
2018-04-27 22:39:25 -0500

    In progress.

--- Variable_Declaration/main.cpp
+++ Variable_Declaration/main.cpp
@@ -2,8 +2,18 @@
 
 int main()
 {
+	bool boolean; //Can be initialized to true/false or 1/0;
+	char character; //Usually initialized to an ASCII character (which cooresponds to an integer); 
 
+	int integer; //Can be initialized to integer value within -(2^32)->(2^32)-1;
+	short int shortInteger; //Can be intitialized to an integer value within -(2^16)->(2^16)-1;
+	long int longInteger; //Can be initialized to an integer value withing -(2^32)->(2^32)-1;
+	long long int longLongInteger; //Can be intialized to an integer value within -(2^64)->(2^64)-1;
+	
+	float numFloat;
 
+	double numDouble;
+	long double numLongDouble;
 
 	return 0;
 }
